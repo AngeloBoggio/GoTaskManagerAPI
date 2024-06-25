8c1166d9a35931a03c1e1afbd8adaae2ee6b4989
AngeloBoggio <gelobogx@gmail.com>
2024-06-24 22:19:21 -0400

    added more env variables. specifically in database.go

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,11 +11,11 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-    dsn := "host=localhost user=taskuser password=taskpassword dbname=taskmanager port=5430 sslmode=disable"
-    database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-    }
+	dsn := os.Getenv("DATA_BASE")
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 
-    DB = database
+	DB = database
 }
